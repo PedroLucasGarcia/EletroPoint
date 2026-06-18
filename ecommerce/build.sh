39ad0d35ae91c1f7814d1d9fbc7ac3b45c140d6c
PedroLucasGarcia <pedrolucasdasilvagarcia@gmail.com>
2026-06-18 10:28:14 +0100

    Add superuser

--- ecommerce/build.sh
+++ ecommerce/build.sh
@@ -4,7 +4,19 @@
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
-
 python manage.py migrate
+
+# Cria o superuser automaticamente se ainda não existir
+python manage.py shell -c "
+from django.contrib.auth.models import User
+import os
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+if username and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print('Superuser criado.')
+else:
+    print('Superuser já existe ou variáveis não definidas.')
+"
